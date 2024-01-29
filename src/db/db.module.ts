de4f2e9909f800a6f0a16681a9bfaaf58325e9d0
Seyyed Mohammad Amir Hekmati <mohammadhekmati1153@gmail.com>
2024-01-29 05:41:35 -0800

    Update db.module.ts

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -2,9 +2,10 @@ import {Module} from "@nestjs/common";
 import {MongooseModule} from "@nestjs/mongoose";
 import * as process from "process";
 import {ConfigModule, ConfigService} from "@nestjs/config";
-
+// mongodb://root:EmcFJJrPZWGtx4aAN6PubfdX@kilimanjaro.liara.cloud:30218/my-app?authSource=admin
+// mongodb://localhost:27017/sinayi
 @Module({
-    imports:[MongooseModule.forRoot("mongodb://localhost:27017/sinayi")]
+    imports:[MongooseModule.forRoot("mongodb://root:EmcFJJrPZWGtx4aAN6PubfdX@kilimanjaro.liara.cloud:30218/my-app?authSource=admin")]
 })
 export class DbModule {
 
